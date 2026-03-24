e4a8ed4fdbc5e1dba7eca1d0a28a76ac4b0655f3
Davy <dfs26@aluno.ifal.edu.br>
2026-03-24 17:32:55 -0300

    testeDavi

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Doctor',
+      title: 'Docto',
       theme: ThemeData(
         // This is the theme of your application.
         //
